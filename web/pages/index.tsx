88e6b6ae73b45f3e34d87f143ce3e2acac212628
joonmarion <marianojoao52@gmail.com>
2022-11-01 11:42:59 -0300

    setup front-end

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-    return <h1>My Blog</h1>;
+interface HomeProps {
+    count: number;
 }
+
+export default function Home(props: HomeProps) {
+    return (
+        <div className="container">
+            <h1>Count: {props.count}</h1>
+        </div>
+    );
+}
+
+export const getServerSideProps = async () => {
+    const response = await fetch('http://localhost:3333/pools/count');
+    const data = await response.json();
+
+    return {
+        props: {
+            count: data,
+        },
+    };
+};
