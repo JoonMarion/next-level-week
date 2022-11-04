53a819c62d6b3be468bffccc8d2bcbb63eaae2b7
joonmarion <marianojoao52@gmail.com>
2022-11-04 12:59:29 -0300

    feat: adding colors project

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
                 sans: 'Roboto, sans-serif',
             },
             colors: {
+                ignite: {
+                    500: '#129e57',
+                },
+                yellow: {
+                    500: '#f7dd43',
+                    700: '#e5cd3d',
+                },
                 gray: {
+                    100: '#e1e1e6',
+                    300: '#8d8d99',
+                    600: '#323238',
+                    800: '#202024',
                     900: '#121214',
                 },
             },
